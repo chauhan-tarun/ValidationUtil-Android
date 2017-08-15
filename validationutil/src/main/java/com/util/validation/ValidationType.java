3ffae32614bdcdcf84e4f2b905800b02b52207dc
pchub <tarunchouhan68@gmail.com>
2017-08-15 10:50:31 +0530

    Generated Validation util structure with comments

---
+++ validationutil/src/main/java/com/util/validation/ValidationType.java
@@ -0,0 +1,15 @@
+package com.util.validation;
+
+/**
+ * ValidationType enum for defining the validation types
+ * that can be used to add validations.
+ * <p>
+ * Use this enum in {@link ValidationUtil} class to add validations
+ * defined in this enum class
+ */
+
+public enum ValidationType {
+
+    NAME, USERNAME, EMAIL, PHONE, PASSWORD, CONFIRM_PASSWORD
+
+}
---
+++ validationutil/src/main/java/com/util/validation/ValidationUtil.java
@@ -0,0 +1,29 @@
+package com.util.validation;
+
+/**
+ * A ValidationUtil library class to help you optimize your code
+ * and easily implement <b>validations</b> on your text fields.
+ * <p>
+ * Simply call {@link #isValid(String, ValidationType)} by passing your text
+ * that needs to be validated and the validation type.
+ * Use {@link ValidationType} class to define the type of your text that
+ * needs to be validated.
+ */
+
+public class ValidationUtil {
+
+    /**
+     * This method will do the validation on text
+     *
+     * @param validationText The text that needs to be validated
+     * @param validationType The {@link ValidationType} to specify the type of validation to be used
+     *                       on th view.
+     * @return Returns the boolean to update whether the validation is success of failed
+     */
+    public static boolean isValid(String validationText, ValidationType validationType) {
+
+
+        return false;
+    }
+
+}
