277df15624719b07ccb1a8c11bface1f59ec4fcc
Tarun Chauhan <tarunchouhan68@gmail.com>
2017-09-29 14:34:11 +0530

    Removed the library module

--- app/src/main/java/com/util/validation/example/MainActivity.java
+++ app/src/main/java/com/util/validation/example/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import com.util.validation.ValidationUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void checkViews(){
+    private void checkViews() {
         EditText et_name, et_email, et_number, et_password;
 
         et_name = (EditText) findViewById(R.id.et_name);
@@ -30,14 +27,5 @@ public class MainActivity extends AppCompatActivity {
         et_number = (EditText) findViewById(R.id.et_number);
         et_password = (EditText) findViewById(R.id.et_password);
 
-        if(!ValidationUtil.isValidName(et_name.getText().toString())){
-            Toast.makeText(this, "Name is not valid", Toast.LENGTH_SHORT).show();
-        }else if(!ValidationUtil.isValidEmail(et_email.getText().toString())){
-            Toast.makeText(this, "Email is not valid", Toast.LENGTH_SHORT).show();
-        }else if(!ValidationUtil.isValidPhone(et_number.getText().toString())){
-            Toast.makeText(this, "Number is not valid", Toast.LENGTH_SHORT).show();
-        }else if(!ValidationUtil.isValidPassword(et_password.getText().toString(),8)){
-            Toast.makeText(this, "Password is not valid", Toast.LENGTH_SHORT).show();
-        }
     }
 }
